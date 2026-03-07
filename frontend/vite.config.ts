d8942612b3940213f9eed25dce1ad4e19906c9d4
CHEVALIER Marc <MaChevalier@ivry94.fr>
2026-03-07 12:51:38 +0100

    feat: Add Docker and Nginx configuration for Proxmox deployment and setup Vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+      '/file_commandes': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+      '/file_factures': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+      '/file_certif': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
